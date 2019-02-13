5a4de5f968b133d52c04aaf7fd3650838d61eafe
Mohammed Sarfarazuddin <42849284+Shahruq@users.noreply.github.com>
2019-02-13 18:01:46 +0530

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -20,3 +20,22 @@ ggplot(mpg, aes(displ,hwy))+geom_point(aes(color = class))
 ggplot(mpg, aes(displ,hwy))+geom_point(aes(size = class))
 
 #alpha - transparency, #shape - different shape
+
+#facet-wrap
+
+ggplot(mpg, aes(displ,hwy))+geom_point(aes(color = class))+facet_wrap(~class, nrow = 2)
+
+#smooth plots instead of points
+ggplot(mpg, aes(displ,hwy))+geom_smooth(aes(color = class))
+
+ggplot(mpg, aes(displ,hwy))+geom_smooth()+geom_point()
+
+##ultimate point and smooth stuff
+
+ggplot(mpg, aes(displ,hwy))+geom_point(aes(color = class))+geom_smooth() #provides a smooth pattern and line through the data points
+ggplot(mpg, aes(displ,hwy))+geom_point(aes(color = class))+geom_smooth(data = filter(mpg, class == "subcompact"), se = F) #smooth only through subcompact cars
+ggplot(mpg, aes(displ,hwy))+geom_point(aes(color = class))+geom_smooth(data = filter(mpg, class == "subcompact"), se = T) #se = T brings along the shade
+
+##bar plots, stat counts and legend
+
+ggplot(mpg, aes(year))+stat_count()
